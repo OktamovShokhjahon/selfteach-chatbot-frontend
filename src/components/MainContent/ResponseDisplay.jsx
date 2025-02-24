e52d1f31d66bfb115bac2ead168586abd4606e7e
Oktamov Shokhjahon <oktamovshokhjahon@gmail.com>
2025-02-24 21:33:50 +0500

    response board style

--- src/components/MainContent/ResponseDisplay.jsx
+++ src/components/MainContent/ResponseDisplay.jsx
@@ -5,6 +5,8 @@ export function ResponseDisplay({ response }) {
 
   if (!response) return null;
 
+  console.log(response);
+
   return (
     <div
       className={`mt-6 md:mt-8 p-4 md:p-6 rounded-lg border ${
@@ -14,7 +16,22 @@ export function ResponseDisplay({ response }) {
       }`}
     >
       <p className={darkMode ? "text-gray-200" : "text-gray-700"}>
-        {response.answer}
+        {response.answer.split("**").map((text, i) =>
+          i % 2 === 0 ? (
+            text.split("\n").map((line, j) => (
+              <span key={`${i}-${j}`} className="">
+                {line}
+                <span className="font-bold">
+                  {j < text.split("\n").length - 1 && <br />}
+                </span>
+              </span>
+            ))
+          ) : (
+            <strong key={i} className="font-bold">
+              {text}
+            </strong>
+          )
+        )}
       </p>
     </div>
   );
